8b851ad01874a61e4d6a100de74a7f869dbf0920
Samuel Yee <samuelyeewl@gmail.com>
2016-08-05 14:04:35 -0700

    Modified config.py to exclude C dependcy of tables module

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+from unittest.mock import MagicMock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -291,3 +292,11 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['tables', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
