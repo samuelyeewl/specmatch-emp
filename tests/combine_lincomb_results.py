f62a8cded92250c4c39b672cc7bbb1f8ad787df0
Samuel Yee <samuelyeewl@gmail.com>
2016-08-14 16:07:37 -0700

    Updated combine lincomb results script

--- tests/combine_lincomb_results.py
+++ tests/combine_lincomb_results.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-import numpy as np
 import pandas as pd
-import glob
 import os
 import sys
 import re
 from argparse import ArgumentParser
 
+from specmatchemp import library
+
 if __name__ == '__main__':
     # Argument parser
-    psr = ArgumentParser(description="Combine lincomb results")
+    psr = ArgumentParser(description="Combine match results")
+    psr.add_argument('libpath', type=str, help="Path to library file")
     psr.add_argument('resdir', type=str, help="Path to results directory")
     psr.add_argument('-s', '--suffix', type=str, default="", help="Suffix to append to results files")
     args = psr.parse_args()
@@ -19,73 +20,44 @@ if __name__ == '__main__':
         print("Could not find folder at {0}".format(args.resdir))
         sys.exit(1)
 
+    lib = library.read_hdf(args.libpath, wavlim='none')
+
     # get names of stars
-    dirs = glob.glob(os.path.join(args.resdir, '*/'))
-    names = [os.path.basename(os.path.normpath(d)) for d in dirs]
+    names = list(lib.library_params.cps_name)
 
     # global dataframe to store 
     res_global = pd.DataFrame()
-    for name, sdir in zip(names, dirs):
-        # individual dataframe for each star
+
+    min_num = 2
+    max_num = 9
+    for name in names:
+        # combine results as one row for each star.
         res_star = pd.DataFrame()
+        for num_best in range(min_num, max_num):
+            res_path = os.path.join(args.resdir, '{0}/{0}_lincomb{1:d}.csv'.format(name, num_best))
 
-        # lincomb num
-        min_num = 2
-        max_num = 9
-        for i in np.arange(min_num, max_num):
-            res_star_num = pd.DataFrame()
-            # get wavelengths used
-            files = glob.glob(os.path.join(sdir, name+'_lincomb{0:d}_*_match.csv'.format(i)))
-            files = [f for f in files if re.search(str(name)+r'_lincomb\d_\d+_match.csv', f)]
-            wls = [re.search(name+r'_lincomb\d_(.+?)_match.csv', f).group(1) for f in files]
-            wls = map(int, wls)
+            # individual dataframe for each and lincomb number
+            res_star_num = pd.read_csv(res_path, index_col=0)
 
-            for wl, f in zip(wls, files):
-                if res_star.empty:
-                    res_star = pd.read_csv(f, index_col=0)
-                    res_star.rename(columns={'lib_index.1':'lib_index'}, inplace=True)
-                    res_star.cps_name = res_star.cps_name.astype(str)
-                    res_star_num = res_star.copy()
-                else:
-                    # merge on columns
-                    df = pd.read_csv(f, index_col=0)
-                    cols = 'best_cs_lincomb{0:d}_{1:d} ref_idxs_lincomb{0:d}_{1:d} coeffs_lincomb{0:d}_{1:d} chi_squared_lincomb{0:d}_{1:d} fit_params_lincomb{0:d}_{1:d}'\
-                    .format(i, wl).split()
-                    res_star = res_star.join(df[cols])
-                
-                    if res_star_num.empty:
-                        res_star_num = df.copy()
-                        res_star_num.rename(columns={'lib_index.1':'lib_index'}, inplace=True)
-                        res_star_num.cps_name = res_star_num.cps_name.astype(str)
-                    else:
-                        # merge on columns
-                        cols = 'best_cs_lincomb{0:d}_{1:d} ref_idxs_lincomb{0:d}_{1:d} coeffs_lincomb{0:d}_{1:d} chi_squared_lincomb{0:d}_{1:d} fit_params_lincomb{0:d}_{1:d}'\
-                        .format(i, wl).split()
-                        res_star_num = res_star_num.join(df[cols])
+            if res_star.empty:
+                res_star = res_star_num
+                res_star['targ_idx'] = res_star.iloc[0].name
+            else:
+                # join on columns
+                cols = [l for l in res_star_num.columns if 'lincomb{0:d}'.format(num_best) in l]
+                res_star[cols] = res_star_num[cols]
 
-            outpath_star_num = os.path.join(sdir, name+'_lincomb{0:d}.csv'.format(i))
-            res_star_num.to_csv(outpath_star_num)
-
-        # save each star's result
-        outpath_star = os.path.join(sdir, name+'_lincomb.csv')
+        # save result for each star
+        outpath_star = os.path.join(args.resdir, '/{0}/{0}_lincomb.csv'.format(name))
         res_star.to_csv(outpath_star)
 
-        # concatenate rows to global dataframe
-        try:
-            targ_idx = res_star[res_star.cps_name.str.contains(name)].index[0]
-            res_star['targ_idx'] = targ_idx
-            if res_global.empty:
-                res_global = res_star
-            else:
-                res_global = pd.concat((res_global, res_star), ignore_index=True)
-        except:
-            print(name)
-
+        # concatenate each star to global dataframe
+        if res_global.empty:
+            res_global = res_star
+        else:
+            res_global = pd.concat((res_global, res_star), ignore_index=True)
 
     # save global results
     res_global.reset_index(inplace=True)
     outpath_global = os.path.join(args.resdir, 'lincomb_results.csv')
     res_global.to_csv(outpath_global)
-
-
-
